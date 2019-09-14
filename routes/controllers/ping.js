4892ea2cc11518fcb916b669a8a7c307c958f522
Jonathan Leigh <jonleigh7@gmail.com>
2019-09-14 19:44:58 -0400

    final

--- routes/controllers/ping.js
+++ routes/controllers/ping.js
@@ -38,23 +38,26 @@ module.exports = (appEnv) => {
 
       });
 
+      function toRad(x) {
+        return x * Math.PI / 180;
+      }
+    
       function findDistance (myLat, myLong, oLat, oLong) { 
 
         let R = 3961;
-        let dlon = oLong - myLong
-        let dlat = oLat - myLat
+        let dlon = toRad(oLong - myLong)
+        let dlat = toRad(oLat - myLat)
        
-        let a = Math.pow(Math.sin(dlat/2), 2) + Math.cos(myLat) * Math.cos(oLat) * Math.pow(Math.sin(dlon/2),2);
+        let a = Math.pow(Math.sin(dlat/2), 2) + Math.cos(toRad(myLat)) * Math.cos(toRad(oLat)) * Math.pow(Math.sin(dlon/2),2);
         let c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a) );
-        let distance = R * c ;
+        let distance = R * c;
 
         return distance;
-        //return Math.sqrt(Math.pow(myLat - oLat, 2) + Math.pow(myLong - oLong, 2));
       }
 
       //console.log(req);
 
-      if( key === "hackmit123456" ){ 
+      if(key === "hackmit123456" ){ 
 
         var date = Date.now();
       
