dffeea32eddce545334e4d50ce0993913ea7bd33
Jonathan Leigh <jonleigh7@gmail.com>
2019-09-14 14:12:13 -0700

    jonathan changes

--- routes/controllers/ping.js
+++ routes/controllers/ping.js
@@ -2,20 +2,55 @@ module.exports = (appEnv) => {
   return {
     pingApi: (req, res) => { 
 
-      var firebase = require("../../util/database");
 
+      const fs = require('fs');
+
+      let rawdata = fs.readFileSync('persistence.json');
+     
 
       //process image by making an api call to google vision 
       var cv = require("../../util/googlevision");
 
       //send the image via base64 text 
 
-      const img = req.image;
-      const location = req.location; 
-      const key = req.key;
-      var date = Date.now(); 
+      const img = req.query.image;
+      const currentLocation = req.query.location; 
+      const key = req.query.key;
 
-      res.send()
+
+      let currentLat = currentLocation.lat; 
+      let currentLong = currentLocation.long; 
+
+      let locationFile = JSON.parse(rawdata);
+
+      let listOfLocations = locationFile.list; 
+      let finalList = []; 
+      
+      listOfLocations.foreach((item, i) => {
+        
+        if (findDistance(item.lat, item.long, currentLat, currentLong)) { 
+          finalList.push([item.lat, item.long]);
+        }
+
+      });
+
+      function findDistance (myLat, myLong, oLat, oLong) { 
+        return Math.sqrt(Math.pow(myLat - oLat, 2) + Math.pow(myLong - oLong, 2));
+      }
+
+      console.log(req);
+
+      if( key === "a123" ){ 
+
+        var date = Date.now();
+      
+        //list of parking spaces lat long 
+        let collegesNearby = [[42.359516,-71.092697], [42.3770068,-71.1188488], [42.359516,-71.092697]];
+        res.send(collegesNearby);
+  
+      } else { 
+        res.send({error:"done"});
+      }
 
     },
     
--- util/database.js
+++ util/database.js
@@ -1,5 +1,11 @@
 // Handles database calls
 
 module.exports = (app, appEnv) => {
-  
+    /*
+  // Import Admin SDK
+    var admin = require("firebase-admin");
+
+    // Get a database reference to our posts
+    return admin.database();
+    */
 }
